484c11b916551250366c468bfa2df291afcd68ca
Will Droste <will.droste@brinqa.com>
2022-10-31 13:44:02 -0500

    Add rebuild index

--- src/main/java/org/neo4j/tool/AbstractIndexCommand.java
+++ src/main/java/org/neo4j/tool/AbstractIndexCommand.java
@@ -11,8 +11,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Config;
 import org.neo4j.driver.Driver;
@@ -217,16 +219,66 @@ abstract class AbstractIndexCommand implements Runnable {
         }
     }
 
+    /** Read all the index and constraints in order, of constrains first. */
     List<IndexData> readIndexes(Driver driver) {
         try (Session session = driver.session()) {
             assert session != null;
             return session.readTransaction(
                     tx -> {
-                        final List<IndexData> indexData = new ArrayList<>();
                         final var result = tx.run("show indexes;");
-                        result.forEachRemaining(record -> indexData.add(fromRecord(record)));
-                        return indexData;
+                        return result.list().stream()
+                                .map(AbstractIndexCommand::fromRecord)
+                                .sorted(new IndexDataComparator())
+                                .collect(Collectors.toList());
                     });
+        }
+    }
+
+    static class IndexDataComparator implements Comparator<IndexData> {
+        private final StringListComparator stringListComparator = new StringListComparator();
+
+        @Override
+        public int compare(IndexData o1, IndexData o2) {
+            // constraints first
+            int cmp = Boolean.compare(o1.isUniqueness(), o2.isUniqueness());
+            if (0 != cmp) {
+                return cmp;
+            }
+            // type
+            cmp = o1.getType().compareTo(o2.getType());
+            if (0 != cmp) {
+                return cmp;
+            }
+            // compare labels
+            cmp = stringListComparator.compare(o1.getLabelsOrTypes(), o2.getLabelsOrTypes());
+            if (0 != cmp) {
+                return cmp;
+            }
+            // name
+            return o1.getName().compareTo(o2.getName());
+        }
+    }
+
+    /** Just sort based on number of items in list first (more complex) to least complex. */
+    static class StringListComparator implements Comparator<List<String>> {
+
+        @Override
+        public int compare(List<String> l1, List<String> l2) {
+            int cmp = Integer.compare(l1.size(), l2.size());
+            if (0 != cmp) {
+                return cmp;
+            }
+
+            final List<String> s1 = l1.stream().sorted().collect(Collectors.toList());
+            final List<String> s2 = l2.stream().sorted().collect(Collectors.toList());
+            for (int i = 0; i < s1.size(); i++) {
+                final String sc1 = s1.get(i);
+                cmp = sc1.compareTo(s2.get(i));
+                if (0 != cmp) {
+                    return cmp;
+                }
+            }
+            return 0;
         }
     }
 
